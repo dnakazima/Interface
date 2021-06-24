d64fa5949259134fb23395426bb1528a720d27e4
Dario Nakazima <dnakazima@hotmail.com>
2021-06-24 09:45:50 -0300

    segunda fase

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,23 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
 
 	}
 
