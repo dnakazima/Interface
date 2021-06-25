d398313c13787afc7f7d6a3a3a0bcabcb7b617b3
Dario Nakazima <dnakazima@hotmail.com>
2021-06-25 11:33:43 -0300

    atualizando...

--- src/model/services/RentalService.java
+++ src/model/services/RentalService.java
@@ -20,6 +20,9 @@ public class RentalService {
 	public void processInvoice(CarRental carRental) {
 		long t1 = carRental.getStart().getTime();
 		long t2 = carRental.getFinish().getTime();
+		
+		//mili * 1000 = sec sec / 60 = min min / 60 = hora
+		
 		double hours = (double)(t2 - t1) / 1000 / 60 / 60;
 		
 		double basicPayment;
